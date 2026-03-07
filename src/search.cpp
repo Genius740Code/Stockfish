a222711618d6d13cc2f99f9230ed6935d53d0bf9
Genius740Code <genius74o@DESKTOP-5E72BNE.localdomain>
2026-03-07 11:34:28 +0000

    a

--- src/search.cpp
+++ src/search.cpp
@@ -1734,7 +1734,9 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
 
 Depth Search::Worker::reduction(bool i, Depth d, int mn, int delta) const {
     int reductionScale = reductions[d] * reductions[mn];
-    return reductionScale - delta * 576 / rootDelta + !i * reductionScale * 217 / 512 + 1182;
+
+    return (reductionScale + 1239 - delta * 795 * 1024 / std::max(1, rootDelta))
+           + (!i && reductionScale > 1341) * 1135;  
 }
 
 // elapsed() returns the time elapsed since the search started. If the
